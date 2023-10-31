d00b87dd3072f4728f26faaab870d0b1502a53db
alexrc11-N <143229102+alexrc11-N@users.noreply.github.com>
2023-10-31 16:53:33 -0500

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,25 @@
+/* 
+ * Author: Alex Rosado
+ *  Challenge 2
+ *  
+ *Contains all the values necessary to run main.cpp correctly.
+ */
+#ifndef FIFO_MAIN_H
+#define FIFO_MAIN_H
 
+#define VALID_STACKSIZE 5
+#define LOWERBOUND 1
+#define TESTVALUE 1000
+#define MULTIPLIER 20
+#define TESTVALUE3 (VALID_STACKSIZE * 1000)
+#define MAXSTRSIZE 20  
+
+#include <iostream> /* cout, endl */
+#include "fifo.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::stoi;
+
+#endif /* FIFO_MAIN_H */
